795a6c4d818bc7719442f62e5b24b803c3c8e3fc
hikey1409 <101631288+hikey1409@users.noreply.github.com>
2023-01-28 23:03:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-price_of_an_item = float(input())
-VAT =float(input())
+price_of_an_item = input()
+VAT = input()
 try:
-  c = (price_of_an_item)*(VAT)/100
+  c = float(price_of_an_item)*float(VAT)/100
   d= c+price_of_an_item
   print(round(d))
 except:
