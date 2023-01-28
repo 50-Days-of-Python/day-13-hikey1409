a851a1b872a5b0045a762ffef975eb1dd095cb1b
hikey1409 <101631288+hikey1409@users.noreply.github.com>
2023-01-28 22:53:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,8 @@
 price_of_an_item = int(input())
 VAT =int(input())
 try:
-  c = price_of_an_item*VAT/100
-except:
-  print("invalid input")
-else:
+  c = (price_of_an_item)*(VAT)/100
   d= c+price_of_an_item
-finally:
   print(int(d))
+except:
+  print("Invalid Input")
