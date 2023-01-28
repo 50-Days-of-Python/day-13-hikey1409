39b0efdb685270906335bbbfe6f41bc171d51944
hikey1409 <101631288+hikey1409@users.noreply.github.com>
2023-01-28 23:09:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+def calculate_price_with_vat(price_of_an_item,VAT):
+    try:
+        vat = float(price_of_an_item) * float(VAT) / 100
+        total_price = float(price_of_an_item) + vat
+        return total_price
+    except:
+        return "Invalid input"
+
 price_of_an_item = input()
 VAT = input()
-try:
-  c = float(price_of_an_item)*float(VAT)/100
-  d= c+price_of_an_item
-  print(round(d))
-except:
-  print("Invalid Input")
+
+total_price = calculate_price_with_vat(price_of_an_item,VAT)
+print(round(total_price))
