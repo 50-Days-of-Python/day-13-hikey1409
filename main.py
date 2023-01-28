b6230f81b75cc34207f892bd6219cfbe9e8faec2
hikey1409 <101631288+hikey1409@users.noreply.github.com>
2023-01-28 22:58:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-#Write your code here.
-price_of_an_item = int(input())
-VAT =int(input())
+price_of_an_item = float(input())
+VAT =float(input())
 try:
   c = (price_of_an_item)*(VAT)/100
   d= c+price_of_an_item
-  print(int(d))
+  print(round(d))
 except:
   print("Invalid Input")
