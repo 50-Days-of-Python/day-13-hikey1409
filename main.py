6149de0dde4ca38f3ebf9bae73619208dd58af59
hikey1409 <101631288+hikey1409@users.noreply.github.com>
2023-01-29 11:38:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ try:
     price= int(input())
     vat=int(intput())
     total= price+price*(vat)/100
-    print(total)
+    print(round(total))
 except:
-     print("Invalid")
+    print("nvalid Input")
